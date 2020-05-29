5a52ed4da53bd310f37f4a5e85276914a348072c
Sebastian Jimenez <sajimenez5@gmail.com>
2020-05-29 11:37:13 -0400

    Added Operation maager and default values

--- src/core/models.py
+++ src/core/models.py
@@ -1,6 +1,20 @@
+import sys
+
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 from django.core.validators import MinValueValidator
+
+
+class OperationManager(models.Manager):
+    def find_code(self, term, amount):
+        qs = self.get_queryset().filter(
+            term_min__lte=term,
+            term_max__gt=term,
+            amount_min__lt=amount,
+            amount_max__gte=amount
+        )
+        if qs.exists():
+            return qs.first().code
 
 
 class Operation(models.Model):
@@ -11,15 +25,16 @@ class Operation(models.Model):
     code = models.CharField(_('code'), max_length=10)
     title = models.CharField(_('title'), max_length=255, null=True, blank=True)
     subtitle = models.CharField(_('subtitle'), max_length=255, null=True, blank=True)
-    term_min = models.PositiveIntegerField(_('term min (inclusive)'), null=True, blank=True)
-    term_max = models.PositiveIntegerField(_('term max'), null=True, blank=True)
-    amount_min = models.FloatField(_('amount min'), validators=[MinValueValidator(0)], null=True, blank=True)
+    term_min = models.PositiveIntegerField(_('minimun term in days (inclusive)'), default=0)
+    term_max = models.PositiveIntegerField(_('maximun term in days'), default=2147483647)
+    amount_min = models.FloatField(_('minimun amount in UF'), validators=[MinValueValidator(0.0)], default=0.0)
     amount_max = models.FloatField(
-        _('amount max (inclusive)'),
-        validators=[MinValueValidator(0)],
-        null=True,
-        blank=True
+        _('maximun amount in UF (inclusive)'),
+        validators=[MinValueValidator(0.0)],
+        default=sys.float_info.max,
     )
+
+    objects = OperationManager()
 
     def __str__(self):
         return self.code
