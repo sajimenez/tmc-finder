22617466937569071e09fba8673a7a155752ddf0
Sebastian Jimenez <sajimenez5@gmail.com>
2020-05-31 11:39:08 -0400

    Fixed local db config

--- src/TmcFinder/settings.py
+++ src/TmcFinder/settings.py
@@ -82,10 +82,15 @@ WSGI_APPLICATION = 'TmcFinder.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-
-DATABASES = {'default': dj_database_url.config(
-    default=f"sqlite:////{os.path.join(BASE_DIR, 'db.sqlite3')}"
-)}
+if os.getenv('LOCAL') == '1':
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
+else:
+    DATABASES = {'default': dj_database_url.config()}
 
 
 # Password validation
