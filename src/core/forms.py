fd728b0c1fdf0dd95286555107c56641187c4630
Sebastian Jimenez <sajimenez5@gmail.com>
2020-05-31 20:03:06 -0400

    Fixed validations

--- src/core/forms.py
+++ src/core/forms.py
@@ -2,7 +2,6 @@ from datetime import date
 from requests.exceptions import Timeout
 
 from django import forms
-from django.core.validators import MinValueValidator
 from django.utils.translation import gettext_lazy as _
 from django.core.exceptions import ValidationError
 from dateutil.relativedelta import relativedelta
@@ -12,25 +11,25 @@ from .utils import get_tmcs_by_date, SbifException
 from .models import Operation
 
 
-def validate_max_date(value):
-    max_date = date.today() + relativedelta(months=1, day=14)
-
-    if value > max_date:
-        raise ValidationError(
-            _('Max date allowed is {}').format(formats.date_format(max_date, 'SHORT_DATE_FORMAT')),
-            params={'value': value},
-        )
-
-
 class LoanForm(forms.Form):
-    term = forms.IntegerField(label=_('Term in days'), validators=[MinValueValidator(0)])
-    amount = forms.FloatField(label=_('Amount in UF'), validators=[MinValueValidator(0.0)])
+    term = forms.IntegerField(label=_('Term in days'), min_value=0, max_value=10000)
+    amount = forms.FloatField(label=_('Amount in UF'), min_value=0.0, max_value=1000000.0)
     date = forms.DateField(
         label=_('Date'),
         initial=date.today,
-        validators=[validate_max_date],
         widget=forms.DateInput(attrs={'placeholder': _('YYYY-MM-DD')}),
     )
+
+    def clean_date(self):
+        value = self.cleaned_data['date']
+        max_date = date.today() + relativedelta(months=1, day=14)
+
+        if value > max_date:
+            raise ValidationError(
+                _('Max date allowed is {}').format(formats.date_format(max_date, 'SHORT_DATE_FORMAT')),
+                params={'value': value},
+            )
+        return value
 
     def find_tmc(self):
         data = self.cleaned_data
